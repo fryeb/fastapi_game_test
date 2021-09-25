7902cf2c89fa34d4b9463e33dc22690f6dc4fb5e
Benjamin Frye <ben.j.frye@gmail.com>
2021-09-25 19:45:00 +1000

    Add templates (and index.html)

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/status")
 async def get_status():
     return ({'status': 'running'})
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {'request': request})
