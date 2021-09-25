ea272c56cc19a6df865688eab9ba4e461b6ff93a
Benjamin Frye <ben.j.frye@gmail.com>
2021-09-25 21:00:04 +1000

    Add create_game

--- server.py
+++ server.py
@@ -1,11 +1,24 @@
+import asyncio
+import uuid
+
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+from pydantic import BaseModel
+from typing import Dict
+
+class Game(BaseModel):
+    name: str
+
+games: Dict[uuid.UUID, Game] = dict()
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/status")
 async def get_status():
@@ -15,3 +28,17 @@ async def get_status():
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {'request': request})
+
+
+@app.post("/")
+async def create_game(request: Request, response_class=RedirectResponse):
+    print("creating game")
+    game_id = uuid.uuid4()
+    games[game_id] = Game(name=f'Game ({game_id})')
+    return RedirectResponse(f'/game/{game_id}', status_code=303)
+
+
+@app.get("/game/{game_id}", response_class=HTMLResponse)
+async def get_game(request: Request, game_id: str):
+    game = games[uuid.UUID(game_id)]
+    return templates.TemplateResponse("game.html", {'request': request})
